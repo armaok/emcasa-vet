3e9dcc7ea0f4149e78b6a70455c4075ec9080ec9
armaok <80077122+armaok@users.noreply.github.com>
2026-04-10 20:36:27 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://emcasa.vet',
-  integrations: [
-    sitemap({
-      changefreq: 'monthly',
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
-  ],
 });
