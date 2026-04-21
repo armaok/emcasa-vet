3ae719b8abc3638697411ad231aa7b264b2a7263
armaok <80077122+armaok@users.noreply.github.com>
2026-04-21 12:51:32 -0300

    Atualizar o astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,10 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://emcasa.vet',
+  vite: {
+    build: {
+      cssCodeSplit: false,
+      assetsInlineLimit: 4096,
+    }
+  }
 });
